4daa186a8b4a58b29c5c2e502f6388598041ba5d
Christian <sword6390@gmail.com>
2022-11-07 13:13:27 -0500

    done

---
+++ js/add-element.js
@@ -0,0 +1,6 @@
+// Testing the class
+
+$('.hot').each(function(){
+    $(this).removeClass("hot");
+    $(this).addClass('cool')
+});
--- js/hotel.js
+++ js/hotel.js
@@ -15,12 +15,29 @@ console.log(test.room);
 // event
 $(function(){
     $('#submit').click(reservation);
+    $('#lists').click(displayR);
 });
 
-function(reservation){
+var guestgroup = new Array();
+function reservation(){
     var guest={
-        name: $('name').val(),
-        numberOfGues: $('nums').val(),
+        name: $('#name').val(),
+        numberOfGuest: $('#nums').val(),
+        checkin: $('#checkin').val(),
+        checkout: $('#checkout').val(),
+
         
+    }
+    $("p:first").html("Thank you for your reservation!");
+    guestgroup.push(guest);  // Store data in array
+    console.log(guest);
+}
+
+function displayR(){
+    $("form").append("<table>");
+    for (var i in guestgroup){
+        var theguest = guestgroup[i];
+        var row = "<tr><td>" + theguest.name + "</td><td>" + theguest.checkin + "</td><td>" + theguest.checkout + "</td><td>" + theguest.numberOfGuest + "</td></tr>";
+        $("table").append(row);
     }
 }
